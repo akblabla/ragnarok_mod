02abf677dacd1085bce7cb2ef08bed44e5884abd
akblabla <Akblabla@gmail.com>
2025-02-11 19:16:46 +0100

    removed unpacked stuff

--- assets_unpacked/pc/binaryFile/lua_-_util_-_linearProgramming.lua
+++
@@ -1,3 +0,0 @@
-local LinearProgram = {}
-
-return LinearProgram
--- assets_unpacked/pc/binaryFile/lua_-_util_-_threat.lua
+++
@@ -1,9 +0,0 @@
-local Wargroove = require "wargroove/wargroove"
-local PosKey = require "util/posKey"
-local Threat = {}
-local threatMap = {}
-function Threat.getThreat(unitClassId,pos)
-	local posKey = PosKey.generatePosKey(pos)
-	return threatMap[posKey]
-end
-return Threat
